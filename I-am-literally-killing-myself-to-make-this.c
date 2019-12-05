88cc063a543611da203f9fd68ac3bef514efadc6
zivoy <ziv132@gmail.com>
2019-12-05 15:48:40 -0500

    we learned something

--- I-am-literally-killing-myself-to-make-this.c
+++ I-am-literally-killing-myself-to-make-this.c
@@ -1,9 +1,25 @@
 #pragma config(Motor,  port1,           motrev,        tmotorVex393_MC29, openLoop)
+#pragma config(Sensor, dgtl1,  bmp,         sensorTouch)
 // get ide here http://www.robotc.net/
 task main(){
-
+	int val = 0;
+	int target = 0;
+	int acc = 7;
 	while(true)
 	{
-			motor[motrev] = 20;  // (y - x)/2
+		if (SensorValue(bmp) == 1){
+			 target = -127;
+		}
+		else{
+			target = 127;
+		}
+		if (val < target){
+			val = val+acc;
+		}
+		else{
+			val = val-acc;
+		}
+		motor[motrev] = val;
+		sleep(10);
 	}
 }
