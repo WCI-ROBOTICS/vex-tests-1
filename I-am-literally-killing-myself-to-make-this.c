a881a67107882c0ddcad8352222d1f548aa9bdc4
zivoy <ziv132@gmail.com>
2019-12-12 16:01:20 -0500

    general moter function

--- I-am-literally-killing-myself-to-make-this.c
+++ I-am-literally-killing-myself-to-make-this.c
@@ -1,25 +1,37 @@
 #pragma config(Motor,  port1,           motrev,        tmotorVex393_MC29, openLoop)
 #pragma config(Sensor, dgtl1,  bmp,         sensorTouch)
 // get ide here http://www.robotc.net/
-task main(){
-	int val = 0;
-	int target = 0;
+// documentation: http://www.robotc.net/tutor/Cortex/cortexunits.php?platform=Cortex
+void goToTarget(float target, short nam){
 	int acc = 7;
-	while(true)
-	{
+	int val = motor[nam];
+
+	while (abs(val - target) < acc){
+			acc = acc/2;
+			if (acc<=1) break;
+	}
+
+	if (val == target){
+		val=val;
+	}
+	else if (val < target){
+		val = val+acc;
+	}
+	else{
+		val = val-acc;
+	}
+	motor[nam] = val;
+}
+
+task main(){
+	while (true){
 		if (SensorValue(bmp) == 1){
-			 target = -127;
+			 goToTarget(-127, motrev);
 		}
 		else{
-			target = 127;
+			goToTarget (127,motrev);
 		}
-		if (val < target){
-			val = val+acc;
-		}
-		else{
-			val = val-acc;
-		}
-		motor[motrev] = val;
-		sleep(10);
+
+		wait1Msec(10);
 	}
 }
