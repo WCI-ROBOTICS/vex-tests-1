983cf7623da3a639d38b7d5ea954555677e577e7
zivoy <ziv132@gmail.com>
2020-01-02 22:54:45 -0500

    finished pid

--- I-am-literally-killing-myself-to-make-this.c
+++ I-am-literally-killing-myself-to-make-this.c
@@ -1,27 +1,33 @@
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 #pragma config(Motor,  port1,           motrev,        tmotorVex393_MC29, openLoop)
 #pragma config(Sensor, dgtl1,  bmp,         sensorTouch)
 // get ide here http://www.robotc.net/
 // documentation: http://www.robotc.net/tutor/Cortex/cortexunits.php?platform=Cortex
 
-int Dt = 10;
+int dt = 10;  // in miliseconds
 
-int Kp = .1;
-int Ki = 0;
-int Kd = .1;
+// controls for pid loop
+float Kp = .1;
+float Ki = 0;
+float Kd = .1;
 
-int previous_error = 0;
-int integral = 0;
+float previous_error = 0;
+float integral = 0;
 
 void goToTarget(float target, short nam){
-	int val = motor[nam];
+	float val = motor[nam];
 
-    error = target - val;
-    integral = integral + error * dt; // dt might have to changed to miliseconds (devide by 1000)
-    derivative = (error - previous_error) / dt;
-    output = Kp * error + Ki * integral + Kd * derivative;
+    float error = target - val;
+    integral = integral + error * dt; // dt might have to changed to seconds (divide by 1000)
+    float derivative = (error - previous_error) / dt;
+    float output = Kp * error + Ki * integral + Kd * derivative;
     previous_error = error;
 
-    motor[nam] = val + output;
+    float next_vel = val + output;
+    next_vel = MAX(-127, next_vel);
+    next_vel = MIN(127, next_vel);
+    motor[nam] = next_vel;
 }
 
 task main(){
@@ -33,6 +39,6 @@ task main(){
 			goToTarget (127,motrev);
 		}
 
-		wait1Msec(Dt);
+		wait1Msec(dt);
 	}
 }
