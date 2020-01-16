656a9641ff0d488b0c72b2d1f4e951f440fbc49f
Ziv Shalit <shalz6613@wrdsb.ca>
2020-01-16 15:53:05 -0500

    redid moters

--- I-am-literally-killing-myself-to-make-this.c
+++ I-am-literally-killing-myself-to-make-this.c
@@ -1,14 +1,13 @@
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
-#pragma config(Motor,  port1,           motrev,        tmotorVex393_MC29, openLoop)
-#pragma config(Sensor, dgtl1,  bmp,         sensorTouch)
+#pragma config(Motor,  port1,           leftMotor,     tmotorVex393,           openLoop, reversed)
+#pragma config(Motor,  port10,          rightMotor,    tmotorVex393,           openLoop, reversed)
 // get ide here http://www.robotc.net/
 // documentation: http://www.robotc.net/tutor/Cortex/cortexunits.php?platform=Cortex
 
 int dt = 10;  // in miliseconds
 
-float start_vel = 0;
-float prev_target = 0;
+
 
 bool inDeadZone(float value1, float value2, float deadZone){
     if (abs(value1 - value2) < deadZone){
@@ -17,7 +16,7 @@ bool inDeadZone(float value1, float value2, float deadZone){
     return false;
 }
 
-void goToTarget(float target, short nam){
+void goToTarget(float target, short nam, float start_vel, float prev_target){
 	float vel = motor[nam];
 
     if (~inDeadZone(target, prev_target, .5)){
@@ -35,16 +34,26 @@ void goToTarget(float target, short nam){
     next_vel = MAX(-127.0, next_vel);
     next_vel = MIN(127.0, next_vel);
     motor[nam] = next_vel;
+	return start_vel, prev_target
 }
 
 task main(){
+	float Lstart_vel = 0;
+	float Lprev_target = 0;
+	float Rstart_vel = 0;
+	float Rprev_target = 0;
+	float slower_modeoffset = 4;
+	
     while (true){
-        if (SensorValue(bmp) == 1){
-            goToTarget(-127, motrev);
-        }
-        else{
-            goToTarget (127,motrev);
-        }
+		float left_target = vexRT[Ch2] + (vexRT[Ch3] / slower_modeoffset);  // left moter forward back
+		float right_target = vexRT[Ch2] + (vexRT[Ch3] / slower_modeoffset);  // right moter forward back
+		
+		left_target = left_target + vexRT[Ch1] + (vexRT[Ch4] / slower_modeoffset);  // left moter turn offset
+		right_target = right_target - vexRT[Ch1] - (vexRT[Ch4] / slower_modeoffset);  // right motar turn offset
+		
+        Lstart_vel, Lprev_target = goToTarget(left_target,  leftMotor,  Lstart_vel, Lprev_target)
+		Rstart_vel, Rprev_target = goToTarget(right_target, rightMotor, Rstart_vel, Rprev_target)
+
 
         wait1Msec(dt);
 	}
