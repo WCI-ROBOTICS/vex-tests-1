14b1eaff61a863a31c20bea6e664dd80e0d4d832
zivoy <ziv132@gmail.com>
2020-01-16 21:00:09 -0500

    now works

--- I-am-literally-killing-myself-to-make-this.c
+++ I-am-literally-killing-myself-to-make-this.c
@@ -8,33 +8,31 @@
 int dt = 10;  // in miliseconds
 
 
-
 bool inDeadZone(float value1, float value2, float deadZone){
-    if (abs(value1 - value2) < deadZone){
-        return true;
-    }
-    return false;
+	if (abs(value1 - value2) < deadZone){
+		return true;
+	}
+	return false;
 }
 
-void goToTarget(float target, short nam, float start_vel, float prev_target){
+void goToTarget(float target, short nam, float *start_vel, float *prev_target){
 	float vel = motor[nam];
 
-    if (~inDeadZone(target, prev_target, .5)){
-        start_vel = vel;
-        prev_target = target;
-    }
+	if (~inDeadZone(target, *prev_target, .5)){
+		*start_vel = vel;
+		*prev_target = target;
+	}
 
-    float cMap = (vel-start_vel)/(target-start_vel);
-    float acl = cMap*(1-cMap);
-    if (~acl && target != vel){
-        acl = .02;
-    }
-    float next_vel = vel + acl*( target-start_vel);
+	float cMap = (vel-*start_vel)/(target-*start_vel);
+	float acl = cMap*(1-cMap);
+	if (~acl && target != vel){
+		acl = .02;
+	}
+	float next_vel = vel + acl*( target-*start_vel);
 
-    next_vel = MAX(-127.0, next_vel);
-    next_vel = MIN(127.0, next_vel);
-    motor[nam] = next_vel;
-	return start_vel, prev_target
+	next_vel = MAX(-127.0, next_vel);
+	next_vel = MIN(127.0, next_vel);
+	motor[nam] = next_vel;
 }
 
 task main(){
@@ -43,18 +41,18 @@ task main(){
 	float Rstart_vel = 0;
 	float Rprev_target = 0;
 	float slower_modeoffset = 4;
-	
-    while (true){
+
+	while (true){
 		float left_target = vexRT[Ch2] + (vexRT[Ch3] / slower_modeoffset);  // left moter forward back
 		float right_target = vexRT[Ch2] + (vexRT[Ch3] / slower_modeoffset);  // right moter forward back
-		
+
 		left_target = left_target + vexRT[Ch1] + (vexRT[Ch4] / slower_modeoffset);  // left moter turn offset
 		right_target = right_target - vexRT[Ch1] - (vexRT[Ch4] / slower_modeoffset);  // right motar turn offset
-		
-        Lstart_vel, Lprev_target = goToTarget(left_target,  leftMotor,  Lstart_vel, Lprev_target)
-		Rstart_vel, Rprev_target = goToTarget(right_target, rightMotor, Rstart_vel, Rprev_target)
+
+		goToTarget(left_target,  leftMotor,  &Lstart_vel, &Lprev_target);
+		goToTarget(right_target, rightMotor, &Rstart_vel, &Rprev_target);
 
 
-        wait1Msec(dt);
+		wait1Msec(dt);
 	}
 }
